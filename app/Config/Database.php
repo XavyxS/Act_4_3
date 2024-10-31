eca490c3716efa759c3f2a212430ae5042fb2872
XavyxS <francisco.sandoval23b@udgvirtual.udg.mx>
2024-10-31 01:04:38 -0600

    Conectando con la DB de Render

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -19,147 +19,24 @@ class Database extends Config
    */
   public string $defaultGroup = 'default';
 
-  /**
-   * The default database connection.
-   *
-   * @var array<string, mixed>
-   */
-  /*     public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => 'postgres',
-        'password'     => 'Compac1984',
-        'database'     => 'DB_test',
-        'DBDriver'     => 'Postgre',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 5432,
-        'numberNative' => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
- */
-  //    /**
-  //     * Sample database connection for SQLite3.
-  //     *
-  //     * @var array<string, mixed>
-  //     */
-  //    public array $default = [
-  //        'database'    => 'database.db',
-  //        'DBDriver'    => 'SQLite3',
-  //        'DBPrefix'    => '',
-  //        'DBDebug'     => true,
-  //        'swapPre'     => '',
-  //        'failover'    => [],
-  //        'foreignKeys' => true,
-  //        'busyTimeout' => 1000,
-  //        'dateFormat'  => [
-  //            'date'     => 'Y-m-d',
-  //            'datetime' => 'Y-m-d H:i:s',
-  //            'time'     => 'H:i:s',
-  //        ],
-  //    ];
-
-  /**
-   * Sample database connection for Postgre.
-   *
-   * @var array<string, mixed>
-   */
   public array $default = [
     'DSN'        => '',
-    'hostname'   => 'localhost',
-    'username'   => 'postgres',
-    'password'   => 'Compac1984',
-    'database'   => 'DB_test',
+    'hostname'   => 'dpg-cshhq72j1k6c73981kb0-a.oregon-postgres.render.com',  
+    'username'   => 'sa',
+    'password'   => 'AtTMisesXcfjOFOCOgoPLpSWcPbp85xW',
+    'database'   => 'db_test_whk5',
     'schema'     => 'public',
     'DBDriver'   => 'Postgre',
     'DBPrefix'   => '',
     'pConnect'   => false,
-    'DBDebug'    => true,
+    'DBDebug'    => (ENVIRONMENT !== 'production'),  
     'charset'    => 'utf8',
+    'DBCollat'   => 'utf8_general_ci',
     'swapPre'    => '',
     'failover'   => [],
     'port'       => 5432,
-    'dateFormat' => [
-      'date'     => 'Y-m-d',
-      'datetime' => 'Y-m-d H:i:s',
-      'time'     => 'H:i:s',
-    ],
-  ];
+];
 
-  //    /**
-  //     * Sample database connection for SQLSRV.
-  //     *
-  //     * @var array<string, mixed>
-  //     */
-  //    public array $default = [
-  //        'DSN'        => '',
-  //        'hostname'   => 'localhost',
-  //        'username'   => 'root',
-  //        'password'   => 'root',
-  //        'database'   => 'ci4',
-  //        'schema'     => 'dbo',
-  //        'DBDriver'   => 'SQLSRV',
-  //        'DBPrefix'   => '',
-  //        'pConnect'   => false,
-  //        'DBDebug'    => true,
-  //        'charset'    => 'utf8',
-  //        'swapPre'    => '',
-  //        'encrypt'    => false,
-  //        'failover'   => [],
-  //        'port'       => 1433,
-  //        'dateFormat' => [
-  //            'date'     => 'Y-m-d',
-  //            'datetime' => 'Y-m-d H:i:s',
-  //            'time'     => 'H:i:s',
-  //        ],
-  //    ];
-
-  //    /**
-  //     * Sample database connection for OCI8.
-  //     *
-  //     * You may need the following environment variables:
-  //     *   NLS_LANG                = 'AMERICAN_AMERICA.UTF8'
-  //     *   NLS_DATE_FORMAT         = 'YYYY-MM-DD HH24:MI:SS'
-  //     *   NLS_TIMESTAMP_FORMAT    = 'YYYY-MM-DD HH24:MI:SS'
-  //     *   NLS_TIMESTAMP_TZ_FORMAT = 'YYYY-MM-DD HH24:MI:SS'
-  //     *
-  //     * @var array<string, mixed>
-  //     */
-  //    public array $default = [
-  //        'DSN'        => 'localhost:1521/XEPDB1',
-  //        'username'   => 'root',
-  //        'password'   => 'root',
-  //        'DBDriver'   => 'OCI8',
-  //        'DBPrefix'   => '',
-  //        'pConnect'   => false,
-  //        'DBDebug'    => true,
-  //        'charset'    => 'AL32UTF8',
-  //        'swapPre'    => '',
-  //        'failover'   => [],
-  //        'dateFormat' => [
-  //            'date'     => 'Y-m-d',
-  //            'datetime' => 'Y-m-d H:i:s',
-  //            'time'     => 'H:i:s',
-  //        ],
-  //    ];
-
-  /**
-   * This database connection is used when running PHPUnit database tests.
-   *
-   * @var array<string, mixed>
-   */
   public array $tests = [
     'DSN'         => '',
     'hostname'    => '127.0.0.1',
